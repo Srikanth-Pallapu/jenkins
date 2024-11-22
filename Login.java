d467e60f23f0066d715df3715eb4629719c0aec0
pavan <abc@gmail.com>
2024-11-22 11:01:01 +0530

    login

---
+++ Login.java
@@ -0,0 +1,17 @@
+import java.net.SocketPermission;
+import java.util.Scanner;
+class Main{
+    public sttaic void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int p[]=new int[n];
+        for(int i=0;i<n;i+++){
+            p[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=p[i];
+        }
+        System.out.println(sum);
+    }
+}
